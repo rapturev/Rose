5e4402ab22bafd8323e406f2422284b2cd1b8484
Kirill Monakhov <withewings0@gmail.com>
2025-08-03 22:01:39 +0300

    Initial commit

---
+++ app/Models/Mirror.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Mirror extends Model
+{
+    /** @use HasFactory<\Database\Factories\MirrorFactory> */
+    use HasFactory;
+}
