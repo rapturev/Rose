cc3dc60f161e515f942bf1629fd651aeb3b07374
Kirill Monakhov <withewings0@gmail.com>
2025-10-05 20:38:03 +0300

    Created PostRequest; Updated PostController; Updated -posts- migration; Updated web-routes; Created index.blade

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
-
+    public function create(PostRequest $request)
+    {
+        Post::create([
+            'title' => $request->title,
+            'content' => $request->content,
+        ]);
+    }
 }
+
--- database/migrations/2025_08_03_185358_create_posts_table.php
+++ database/migrations/2025_08_03_185358_create_posts_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('title');
             $table->text('content');
-            $table->datetime('posted_at');
             $table->timestamps();
         });
     }
