7fd08449cceafc94e091a43f5aa2f075fa2a9189
Kirill Monakhov <withewings0@gmail.com>
2025-10-05 21:27:55 +0300

    Updated web-routes

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/feed', [PostFeedController::class, 'index']);
 
+Route::get('/posts', [PostController::class, 'index']);
+Route::get('/posts/create', [PostController::class, 'create']);
 Route::post('/posts', [PostController::class, 'store']);
 Route::get('/posts/{id}', [PostController::class, 'show']);
-
-
+Route::get('/posts/{id}/edit', [PostController::class, 'edit']);
+Route::patch('/posts/{id}', [PostController::class, 'update']);
+Route::delete('/posts/{id}', [PostController::class, 'destroy']);
