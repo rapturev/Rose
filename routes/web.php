e56728d6a601a54b3cbf31f8bb52eddf5bc53947
Kirill Monakhov <withewings0@gmail.com>
2025-10-05 21:14:51 +0300

    Updated web-routes

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/feed', [PostFeedController::class, 'index']);
 
+Route::post('/posts', [PostController::class, 'store']);
+Route::get('/posts/{id}', [PostController::class, 'show']);
+
+
