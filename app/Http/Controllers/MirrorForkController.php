3640497c7756013fbfc5d2e51ca27211a735b5ec
Kirill Monakhov <withewings0@gmail.com>
2025-10-09 21:37:24 +0300

    Updated controllers

--- app/Http/Controllers/MirrorForkController.php
+++ app/Http/Controllers/MirrorForkController.php
@@ -6,5 +6,40 @@ use Illuminate\Http\Request;
 
 class MirrorForkController extends Controller
 {
-    //
+    public function index()
+    {
+
+    }
+
+    public function create()
+    {
+
+    }
+
+    public function store()
+    {
+
+    }
+
+    public function show()
+    {
+
+    }
+
+    public function edit()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function destroy()
+    {
+
+    }
+
+
 }
--- app/Http/Controllers/PathGroupController.php
+++ app/Http/Controllers/PathGroupController.php
@@ -6,5 +6,40 @@ use Illuminate\Http\Request;
 
 class PathGroupController extends Controller
 {
-    //
+    public function index()
+    {
+
+    }
+
+    public function create()
+    {
+
+    }
+
+    public function store()
+    {
+
+    }
+
+    public function show()
+    {
+
+    }
+
+    public function edit()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function destroy()
+    {
+
+    }
+
+
 }
